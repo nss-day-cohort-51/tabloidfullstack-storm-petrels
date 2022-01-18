3c6d069daed5c676a55a2adc2ce2cb5a965a603a
Kaitlyn Long <katielong11@gmail.com>
2022-01-18 13:44:51 -0600

    created models for Post and Category

---
+++ Tabloid/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string ImageLocation { get; set; }
+        public DateTime CreateDateTime { get; set; }
+        public DateTime? PublishDateTime { get; set; }
+        public bool IsApproved { get; set; }
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int UserProfileID { get; set; }
+        public UserProfile UserProfile { get; set; }
+    }
+}
