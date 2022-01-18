d3ed489d39306f2dcaaf244533037699b4503e0c
scrawleyy1 <crawley.sydney@gmail.com>
2022-01-18 13:37:54 -0800

    added the post repo and controller

--- Tabloid/Repositories/PostRepository.cs
+++ Tabloid/Repositories/PostRepository.cs
@@ -1,11 +1,80 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tabloid.Models;
+using Tabloid.Utils;
 
 namespace Tabloid.Repositories
 {
-    public class PostRepository
+    public class PostRepository : BaseRepository, IPostRepository
     {
+        public PostRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<Post> GetAll()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT p.Id, p.Title, p.Content, p.ImageLocation,
+		            p.CreateDateTime, p.PublishDateTime, p.IsApproved,
+		            p.CategoryId, p.UserProfileId,
+		            u.Id AS UserId, u.DisplayName, u.FirstName, u.LastName, u.Email,
+		            u.ImageLocation AS UserImage, u.UserTypeId, ut.[Name]
+                        FROM    Post p
+                        LEFT JOIN UserProfile u
+                        ON p.UserProfileId = u.Id
+                        LEFT JOIN UserType ut
+                        ON u.UserTypeId = ut.Id
+                        ORDER BY PublishDateTime desc";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        var posts = new List<Post>();
+                        while (reader.Read())
+                        {
+                            posts.Add(new Post()
+                            {
+                                Id = DbUtils.GetInt(reader, "Id"),
+                                Title = DbUtils.GetString(reader, "Title"),
+                                Content = DbUtils.GetString(reader, "Content"),
+                                ImageLocation = DbUtils.GetString(reader, "ImageLocation"),
+                                CreateDateTime = DbUtils.GetDateTime(reader, "CreateDateTIme"),
+                                PublishDateTime = DbUtils.GetDateTime(reader, "PublishDateTime"),
+                                IsApproved = true,
+                                CategoryId = DbUtils.GetInt(reader, "CategoryId"),
+                                Category = new Category()
+                                {
+                                    Id = DbUtils.GetInt(reader, "Id"),
+                                    Name = DbUtils.GetString(reader, "Name"),
+                                },
+                                UserProfileID = DbUtils.GetInt(reader, "UserProfileId"),
+                                UserProfile = new UserProfile()
+                                {
+                                    Id = DbUtils.GetInt(reader, "UserId"),
+                                    DisplayName = DbUtils.GetString(reader, "DisplayName"),
+                                    FirstName = DbUtils.GetString(reader, "FirstName"),
+                                    LastName = DbUtils.GetString(reader, "LastName"),
+                                    Email = DbUtils.GetString(reader, "Email"),
+                                    ImageLocation = DbUtils.GetString(reader, "UserImage"),
+                                    UserTypeId = DbUtils.GetInt(reader, "UserTypeId"),
+                                    UserType = new UserType()
+                                    {
+                                        Name = DbUtils.GetString(reader, "Name"),
+                                        Id = DbUtils.GetInt(reader, "UserTypeId")
+                                    }
+                                }
+                            });
+                        }
+                        return posts;
+                    }
+                }
+            }
+        }
     }
 }
