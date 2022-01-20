997476beaa7d894af7a19b451477a0f10b4ef342
joshuastewart417 <joshua.stewart780@gmail.com>
2022-01-20 09:05:15 -0600

    checkout to kl branch

--- Tabloid/Repositories/CategoryRepository.cs
+++ Tabloid/Repositories/CategoryRepository.cs
@@ -42,5 +42,24 @@ namespace Tabloid.Repositories
                 }
             }
         }
+
+        public void Add(Category category)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Category (Name);
+                        OUTPUT INSERTED.ID
+                        VALUES (@Name)";
+                    DbUtils.AddParameter(cmd, "@Name", category.Name);
+
+                    category.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
     }
 }
